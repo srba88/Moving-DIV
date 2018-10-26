3ac8360a912ca16e13994dce56f6aa4aaea08232
srba88 <srba.sokolovic@gmail.com>
2018-10-26 05:22:26 +0200

    second commit

--- main.js
+++ main.js
@@ -1,22 +1,36 @@
-var box = document.getElementById('box');
 
-addEventListener('keydown', function(e){
-    if (e.which == 37) {
-    	box.style.left = '40px';
-    };
-    if (e.which == 38) {
-    	box.style.top = '40px';
-    };
-    if (e.which == 39) {
-    	box.style.left = '900px';
-    };
-    if (e.which == 40) {
-    	box.style.top = '400px';
-    };
-});
 
-box.addEventListener('click', clear);
-
-function clear(){
-	this.style.display = 'none';
+   window.onload = function(){
+    var box = document.getElementById('box');
+    var top = 0;
+    var left = 0;
+    top = +top;
+    left = +left;
+    addEventListener('keydown', function(e){
+   switch(e.which){
+       case 37:
+        left -= 10;
+        box.style.left = left +'px';
+       break;
+       case 38:
+        top -= 10;
+        box.style.top = top +'px';
+       break;
+        case 39:
+        left += 10;
+        box.style.left = left +'px';
+       break;
+       case 40:
+        top += 10;
+        box.style.top = top +'px';
+       break;
+    
+   }
+   
+    })
+    box.addEventListener('click', clear);
 }
+function clear(){
+        this.style.display = 'none';
+}
+
